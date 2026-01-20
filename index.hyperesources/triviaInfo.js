9d571728c7b5a91ebfed06f4a1dafd97a6efbe12
suzrei <ncuffie@outlook.com>
2026-01-20 15:51:55 -0600

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,44 @@
+function getInfo() {
+	window.numberOfQuestions = 	5;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"US President Trivia";
+	window.questions = [		"The first President of the United States to die while in office was", 
+								"John Wilkes Booth shouted the following latin phrase after shooting President Lincoln",
+								"The only person to serve as both the President and on the Supreme Court was", 
+								"The creation of the Interstate Highway System is primarily credited to", 
+								"The formation of The League of Nations was strongly promoted by" ];
+	window.correctAnswer = [ 	"D", "C", "A", "B", "E" ]; 
+	window.answerA = [ 			"Zachary Taylor", 
+								"In Vino Veritas", 
+								"William Howard Taft", 
+								"Harry Truman", 
+								"Benjamin Harrison" ];
+	window.answerB = [ 			"James K Polk", 
+								"E Pluribus Unum", 
+								"Calvin Collidge", 
+								"Dwight D. Eisenhower",
+								"Grover Cleveland" ];
+	window.answerC = [ 			"John Tyler",
+								"Sic Semper Tyrannis",
+								"Warren G Harding", 
+								"Herbert Hoover", 
+								"William McKinley" ];
+	window.answerD = [ 			"William Henry Harrison",  
+								"Alea Lacta Est",
+								"Woodrow Wilson", 
+								"John F Kennedy",
+								"Chester A Arthur" ];
+	window.answerE = [ 			"Martin Van Buren", 
+								"Cogito Ergo Sum", 
+								"William McKinley", 
+								"Franklin D. osevelt", 
+								"Woodrow Wilson" ];
+	window.response = [ 		"William Henry Harrison died on April 4, 1841 after only serving 30 days in office", 
+								"Sic Semper Tyrannis roughly translates to 'Thus Always To Tyrants'", 
+								"Taft was strongly endorsed for the Presidency by Theodore Roosevelt, but the two later had a falling out", 
+								"Eisenhower modeled the US system off of the German Reichsautobahn system created during World War II", 
+								"The League of Nations lasted 26 years and was eventually replaced by The United Nations" ];
+							
+}
+						
